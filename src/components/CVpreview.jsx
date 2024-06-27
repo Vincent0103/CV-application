30cb11ceac78b5f5251e0bc883b2057d04ec849e
Vincent0103 <vincentkanagasundaram@gmail.com>
2024-06-27 18:09:36 +0200

    Begin building UI

---
+++ src/components/CVpreview.jsx
@@ -0,0 +1,7 @@
+const CVpreview = () => {
+  return (
+    <div className="bg-slate-100 w-[21cm] h-[29.7cm] max-h-full"></div>
+  );
+}
+
+export default CVpreview;
---
+++ src/components/FormSections.jsx
@@ -0,0 +1,10 @@
+const FormSections = () => {
+  return (
+    <div className="w-[40%] flex flex-col">
+      <div className="bg-indigo-700 rounded-lg h-10 max-w-full">
+      </div>
+    </div>
+  );
+}
+
+export default FormSections;
