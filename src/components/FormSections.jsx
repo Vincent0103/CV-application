fb46c9e9ddf706952b108e395075878e4101d4cc
Vincent0103 <vincentkanagasundaram@gmail.com>
2024-06-28 10:50:30 +0200

    Add forms top buttons

--- src/components/FormSections.jsx
+++ src/components/FormSections.jsx
@@ -1,7 +1,10 @@
 const FormSections = () => {
   return (
     <div className="w-[40%] flex flex-col">
-      <div className="bg-indigo-700 rounded-lg h-10 max-w-full">
+      <div className="rounded-lg h-10 max-w-full flex justify-center items-end gap-3">
+        <button type="button" className="font-bold text-3xl">General</button>
+        <button type="button" className="border-x-2 border-gray-300 px-3 h-7 flex items-end">Education</button>
+        <button type="button" className="">Experiences</button>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        text: "#eeeaf1",
+        background: "#130f17",
+        primary: "#B8A4C5",
+        secondary: "#6A4344",
+        accent: "#A99178",
+      }
+    },
   },
   plugins: [],
 }
